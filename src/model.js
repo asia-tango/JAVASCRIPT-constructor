375caccaa1170fb17a5e989708ac7feec78b8dd8
Oleksandra Kaplun <kaplunoleksandra@gmail.com>
2020-08-22 14:01:30 +0300

    Optimization and refactoring

--- src/model.js
+++ src/model.js
@@ -1,10 +1,17 @@
 export const model = [
-  {type: 'title', value: 'Test Title', options: {}},
-  {type: 'text', value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, amet. Saepe consequuntur temporibus a quam in obcaecati nostrum soluta aliquam.', options: {}},
-  {type: 'text-columns', value: [
+  {type: 'title', value: 'Test Title', options: {
+    tag: 'h2',
+    styles: 'background: darkred; color: white; text-align: center;'
+  }},
+  {type: 'text', value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, amet. Saepe consequuntur temporibus a quam in obcaecati nostrum soluta aliquam.', options: {
+    styles: 'background: darkgreen; color: yellow;'
+  }},
+  {type: 'textColumns', value: [
     '1 text',
     '2 text',
-    '3 text',
-    '4 text'
-  ], options: {}}
+    '3 text'
+  ], options: {
+    styles: 'padding: 1rem; background: violet;'
+  }},
+  
 ]
